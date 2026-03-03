0fe791d1ce51287ae930cfee4abecfa79bf21a61
Ubuntu <dops@devops-vm.0yckqu3m2wie5jxpfsbjyi4f1d.ix.internal.cloudapp.net>
2026-03-03 16:36:47 +0000

    Converted to docker-compose deployment

--- deploy.sh
+++ deploy.sh
@@ -3,29 +3,10 @@
 echo "Pulling latest code..."
 git pull origin main
 
-echo "Stopping old containers..."
-docker stop frontend-container || true
-docker stop backend-container || true
+echo "Stopping existing containers..."
+docker compose down
 
-docker rm frontend-container || true
-docker rm backend-container || true
+echo "Building and starting containers..."
+docker compose up -d --build
 
-echo "Removing old images..."
-docker rmi ecommerce-frontend || true
-docker rmi ecommerce-backend || true
-
-echo "Building backend..."
-cd backend
-docker build -t ecommerce-backend .
-
-echo "Building frontend..."
-cd ../frontend
-docker build -t ecommerce-frontend .
-
-echo "Starting backend..."
-docker run -d -p 8000:8000 --name backend-container ecommerce-backend
-
-echo "Starting frontend..."
-docker run -d -p 3000:3000 --name frontend-container ecommerce-frontend
-
-echo "Deployment Completed Successfully 🚀"
+echo "Deployment completed successfully 🚀"
