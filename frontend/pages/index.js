9efd831da9df4fd3d09b24f3e227eac65baa07bf
Ubuntu <dops@devops-vm.0yckqu3m2wie5jxpfsbjyi4f1d.ix.internal.cloudapp.net>
2026-03-03 16:22:20 +0000

    Reverted CI/CD test change

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,15 @@
+import FeatureProducts from "Components/Products/FeatureProducts";
+import RecentProducts from "Components/Products/RecentProducts";
+import TopSellings from 'Components/Products/TopSellings'
+import ProductByCategory from "Components/Products/ProductByCategory"
+
 export default function Home() {
   return (
-    <div style={{textAlign: "center", marginTop: "50px"}}>
-      <h1>CI/CD TEST SUCCESS 🚀</h1>
+    <div className=''>
+      <FeatureProducts />
+      <RecentProducts />
+      <TopSellings />
+      <ProductByCategory />
     </div>
   )
 }
